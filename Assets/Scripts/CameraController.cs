64d2ca15a850d69f3085fbbcdb2434ab1d0eb146
JackTYM <jacksonkyarger@gmail.com>
2022-10-26 19:52:26 -0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    float xSensitivity = 3f;
+    float ySensitivity = 1f;
+
+    float xOffset = 0f;
+    float yOffset = 0.5f;
+    float zOffset = 0f;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(xOffset, yOffset, zOffset);
+
+        float mouseXMovement = Input.GetAxis("Mouse X");
+        float mouseYMovement = Input.GetAxis("Mouse Y");
+
+        player.transform.eulerAngles = player.transform.eulerAngles - new Vector3(0, -mouseXMovement * xSensitivity, 0);
+        transform.eulerAngles = transform.eulerAngles - new Vector3(mouseYMovement * ySensitivity, -mouseXMovement * xSensitivity, 0);
+    }
+}
