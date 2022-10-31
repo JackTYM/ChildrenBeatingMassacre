5cb698e3b9f0269380c858f0b866279b37117ba9
Arnav <arnanduri2006@gmail.com>
2022-10-31 00:22:58 -0400

    Added Health Bar
    Added a circular health bar system, will be updated and revamped in later updates

--- Assets/Scripts/HealthUIHandler.cs
+++ Assets/Scripts/HealthUIHandler.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthUIHandler : MonoBehaviour
 {
-
+    Slider sd;
     Text textBox;
     HealthController hc;
 
     // Start is called before the first frame update
     void Start()
     {
-        textBox = GetComponent<Text>();
+        textBox = transform.Find("Health Number").GetComponent<Text>();
         hc = GameObject.Find("Player").GetComponent<HealthController>();
+        sd = GetComponent<Slider>();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if (hc.health == 0) {
-            textBox.text = "You died.";
-        } else {
-            textBox.text = "Health: " + hc.health + "/" + hc.maxHealth;
-        }
+    {
+        sd.value = hc.health / hc.maxHealth * 100;
+        textBox.text = "" + hc.health;
+        
     }
 }
